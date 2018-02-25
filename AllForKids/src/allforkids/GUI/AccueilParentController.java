0da55b960be8112121e3280a2f2f96feddfba414
houaida <houaida.meknassi@esprit.tn>
2018-02-25 12:03:50 +0100

    co

--- AllForKids/src/allforkids/GUI/AccueilParentController.java
+++ AllForKids/src/allforkids/GUI/AccueilParentController.java
@@ -95,7 +95,7 @@ public class AccueilParentController implements Initializable {
 
        
 
-<<<<<<< HEAD
+
       /*  imgView.setImage(new Image("file:/Users/MacBook/Desktop/3A5/PIDEV/AllForKids/Utopia/AllForKids/src/allforkids/GUI/img/hodahadher/garderie1.jpg"));
        
         imgView1.setImage(new Image("file:/Users/MacBook/Desktop/3A5/PIDEV/AllForKids/Utopia/AllForKids/src/allforkids/GUI/img/hodahadher/boutique1.jpg"));
@@ -118,13 +118,13 @@ public class AccueilParentController implements Initializable {
      video.setFitHeight(400);
      video.setFitWidth(700);
      video.setMediaPlayer(mediaplayer);
-<<<<<<< HEAD
+
       mediaplayer.setAutoPlay(true);*/
      
-=======
-      mediaplayer.setAutoPlay(true);
 
->>>>>>> 8e8aa203b9185b8f18b5a9b2e99eda9d8fad9847
+      
+
+
     }
 
     
